d24bd18881032066a24b248c5fbcf87903729274
aref <aref@example.com>
2023-11-30 22:32:43 +0330

    react-query

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Suspense } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 // ** Router Import
 import Router from "./router/Router";
 
 const App = () => {
+
+  const client = new QueryClient({defaultOptions: {queries: {refetchOnWindowFocus: false, staleTime: 1000*6*5}, mutations:{}}})
+
   return (
-    <Suspense fallback={null}>
-      <Router />
-    </Suspense>
+    <QueryClientProvider client={client}>
+      <Suspense fallback={null}>
+        <Router />
+      </Suspense>
+    </QueryClientProvider>
+
   );
 };
 
 export default App;
+
