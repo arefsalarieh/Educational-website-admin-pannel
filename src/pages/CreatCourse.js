eedf920077d0af1014cf65fd9261fa2795524eba
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2023-12-03 23:00:19 +0330

    ffdd

---
+++ src/pages/CreatCourse.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormCreatCourse from '../@core/components/FormCreatCourse/FormCreatCourse'
+
+function CreatCourse() {
+  return (
+    <div>
+      <FormCreatCourse/>
+    </div>
+  )
+}
+
+export default CreatCourse
