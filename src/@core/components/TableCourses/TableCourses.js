df576a926a4c68bd239c26f609f672ce7cac58b3
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2023-12-03 23:00:11 +0330

    fdfd

--- src/@core/components/TableCourses/TableCourses.js
+++ src/@core/components/TableCourses/TableCourses.js
@@ -7,54 +7,71 @@ import {
   DropdownMenu,
   DropdownItem,
   DropdownToggle,
+  Button,
 } from "reactstrap";
 import http from "../../interceptor";
 import { useQuery } from "react-query";
 import CourseItem from "./CourseItem";
+import { useNavigate } from "react-router-dom";
 
 const TableCourses = () => {
-
-
-  const getAllCourses = async () =>{
-    const result = await http.get(`/Course/CourseList?PageNumber=1&RowsOfPage=10&SortingCol=DESC&SortType=Expire&Query`)
+  const getAllCourses = async () => {
+    const result = await http.get(
+      `/Course/CourseList?PageNumber=1&RowsOfPage=10&SortingCol=DESC&SortType=Expire&Query`
+    );
     return result;
-  }
+  };
 
-  const {data , status} = useQuery("getAllCourses", getAllCourses);
+  const { data, status } = useQuery("getAllCourses", getAllCourses);
+
+  const navigate = useNavigate();
 
   return (
-    <Table responsive>
-      <thead>
-        <tr>
-          <th>نام دوره</th>
-          <th>عنوان دوره</th>
-          <th>سطح دوره</th>
-          <th>نام کلاس</th>
-          <th>وضعیت دوره</th>
-          <th>نوع دوره</th>
-          <th>قیمت</th>
-          <th>توضیحات </th>
-        </tr>
-      </thead>
-      <tbody>
-        {status === "success" &&
-          data.courseDtos.map((item, index) => {
-            return (
-              <CourseItem
-                key={index}
-                fullName={item.fullName}
-                typeName={item.typeName}
-                statusName={item.statusName}
-                classRoomName={item.classRoomName}
-                levelName={item.levelName}
-                cost={item.cost}
-                title={item.title}
-                describe={item.describe}
-              />
-            );
-          })}
-      </tbody>
-    </Table>
+    <>
+      <Button
+        className="me-1 mb-4 mt-2"
+        color="primary"
+        type="submit"
+        onClick={() => {
+          navigate("/CreatCourse");
+        }}
+      >
+        ایجاد دوره جدید
+      </Button>
+
+      <Table responsive>
+        <thead>
+          <tr>
+            <th  className='text-nowrap '>نام دوره</th>
+            <th  className='text-nowrap '>عنوان دوره</th>
+            <th  className='text-nowrap '>سطح دوره</th>
+            {/* <th  className='text-nowrap '>شماره کلاس</th> */}
+            <th  className='text-nowrap '>وضعیت دوره</th>
+            <th  className='text-nowrap '>نوع دوره</th>
+            <th  className='text-nowrap '>قیمت</th>
+            {/* <th>توضیحات </th> */}
+          </tr>
+        </thead>
+        <tbody>
+          {status === "success" &&
+            data.courseDtos.map((item, index) => {
+              return (
+                <CourseItem
+                  key={index}
+                  fullName={item.fullName}
+                  typeName={item.typeName}
+                  statusName={item.statusName}
+                  // classRoomName={item.classRoomName}
+                  levelName={item.levelName}
+                  cost={item.cost}
+                  title={item.title}
+                  // describe={item.describe}
+                />
+              );
+            })}
+        </tbody>
+      </Table>
+    </>
   );
 };
 
