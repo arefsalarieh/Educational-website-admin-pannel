ef8cc1ec80c32696864616d30d04a1f013a49dfc
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2023-12-05 11:25:39 +0330

    cjjgj

--- src/@core/components/FormCreatCourse/FormCreatCourse.js
+++ src/@core/components/FormCreatCourse/FormCreatCourse.js
@@ -99,11 +99,11 @@ const FormCreatCourse = () => {
             <CardTitle tag="h4">ایجاد دوره جدید </CardTitle>
           </CardHeader>
           <CardBody>
-            {/* {({ values, handleSubmit, handleChange }) => (
-              <Form  onSubmit={handleSubmit}> */}
+            {({ values, handleSubmit, handleChange }) => (
+              <form  onSubmit={handleSubmit}>
                 <SelectOptions/>
-              {/* </Form>
-            )} */}
+              </form>
+            )}
             <div className="d-flex">
               <Button className="me-1" color="primary" type="submit">
                 اضافه کردن
