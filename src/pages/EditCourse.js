bb39009791fac09372361e3c195cdf09e08f880f
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2023-12-07 11:56:44 +0330

    fsdsf

---
+++ src/pages/EditCourse.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormEditCourse from '../@core/components/FormCreatCourse/FormEditCourse'
+
+function CreatCourse() {
+  return (
+    <div>
+      <FormEditCourse/>
+    </div>
+  )
+}
+
+export default CreatCourse
