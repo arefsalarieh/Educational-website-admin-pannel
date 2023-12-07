4c1b869159c13da82a4ae99f509a108183806702
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2023-12-08 02:27:38 +0330

    dfasfd

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -14,29 +14,23 @@ export default [
     navLink: "/second-page",
   },
   {
-    id: "smaplePage",
+    id: "CourseList",
     title: " دوره های آموزشی",
     icon: <Airplay size={20} />,
     // navLink: "/sample",
     children: [
-      {
-        id: "invoiceList",
-        title: "List",
-        icon: <Circle size={12} />,
-        navLink: "/apps/invoice/list",
-      },
       {
         id: "courdseList",
         title: "لیست دوره ها",
         icon: <List  size={12} />,
         navLink: "/CoursesList",
       },
-      {
-        id: "courdseList",
-        title: "222لیست دوره ها",
-        icon: <List  size={12} />,
-        navLink: "/UsersList",
-      },
+      // {
+      //   id: "courdseList",
+      //   title: "222لیست دوره ها",
+      //   icon: <List  size={12} />,
+      //   navLink: "/UsersList",
+      // },
     ],
   },
   {
