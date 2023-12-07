e85c4ca962a5acf5649e4513f11c6570f10718c5
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2023-12-07 11:57:44 +0330

    sdfsdff

--- src/@core/components/FormCreatCourse/FormCreatCourse.js
+++ src/@core/components/FormCreatCourse/FormCreatCourse.js
@@ -41,20 +41,34 @@ const FormCreatCourse = () => {
   });
 
   // ** Hooks
-  const {
-    reset,
-  } = useForm({ mode: "onChange" });
+  const { reset } = useForm({ mode: "onChange" });
 
   const handleReset = () => {
     reset({
-      title: "",
+      Title: "",
       technology: "",
       status: "",
-      level: "",
-      type: "",
-      term: "",
-      capacity: "",
-      describe: "",
+      CourseLvlId: "",
+      CourseTypeId: "",
+      TremId: "",
+      Describe: "",
+      MiniDescribe:"",
+      Capacity:"",
+      CourseTypeId:"",
+      SessionNumber:"",
+      CurrentCoursePaymentNumber:"",
+      ClassId:"",
+      CourseLvlId:"",
+      TeacherId:"",
+      Cost:"",
+      StartTime:"",
+      EndTime:"",
+      GoogleSchema:"",
+      GoogleTitle:"",
+      CoursePrerequisiteId:"",
+      ShortLink:"",
+      TumbImageAddress:"",
+      ImageAddress:""
     });
   };
 
@@ -62,63 +76,92 @@ const FormCreatCourse = () => {
     const dataForm = new FormData();
 
     const setCourses = {
-      // CourseId :values.id,
-      // Title : values.title,
-      // Describe : values.desc,
+       Title :values.Title,
+       Describe : values.Describe,
+       MiniDescribe : values.MiniDescribe,
+       Capacity :values.Capacity,
+       CourseTypeId : values.CourseTypeId,
+       SessionNumber : values.SessionNumber,
+       CurrentCoursePaymentNumber :values.CurrentCoursePaymentNumber,
+       TremId : values.TremId,
+       ClassId : values.ClassId,
+       CourseLvlId :values.CourseLvlId,
+       TeacherId : values.TeacherId,
+       Cost : values.Cost,
+       UniqeUrlString :values.UniqeUrlString,
+       ShortLink : values.ShortLink,
+       TumbImageAddress : values.TumbImageAddress,
+       ImageAddress :values.ImageAddress,
     };
     const keys = Object.keys(setCourses);
     keys.forEach((key) => {
       const item = setCourses[key];
-      data.append(key, item);
-      //console.log(data);
+      dataForm.append(key, item);
+      console.log(data);
     });
-    const res = await http.post(`/Course`,dataForm);
+    const res = await http.post(`/Course`, dataForm);
     return res;
   };
 
-  const { data, status } = useQuery("CreatCourse", CreatCourse);
 
   {
     return (
       <Formik
         initialValues={{
-          title:"",
-          technology:"",
-          status: "",
-          level: "",
-          type: "",
-          term: "",
-          capacity: "",
-          describe:"",
+          Title: "",
+          technology: "",
+          // status: "",
+          CourseLvlId: "",
+          CourseTypeId: "",
+          TremId: "",
+          Describe: "",
+          MiniDescribe:"",
+          Capacity:"",
+          CourseTypeId:"",
+          SessionNumber:"",
+          CurrentCoursePaymentNumber:"",
+          ClassId:"",
+          CourseLvlId:"",
+          TeacherId:"",
+          Cost:"",
+          StartTime:"",
+          EndTime:"",
+          GoogleSchema:"",
+          GoogleTitle:"",
+          CoursePrerequisiteId:"",
+          ShortLink:"",
+          TumbImageAddress:"",
+          ImageAddress:""
         }}
         validationSchema={validation}
         onSubmit={CreatCourse}
       >
-        <Card>
-          <CardHeader>
-            <CardTitle tag="h4">ایجاد دوره جدید </CardTitle>
-          </CardHeader>
-          <CardBody>
-            {/* {({ values, handleSubmit, handleChange }) => (
-              <form  onSubmit={handleSubmit}> */}
-                <SelectOptions/>
-              {/* </form>
-            )} */}
-            <div className="d-flex">
-              <Button className="me-1" color="primary" type="submit">
-                اضافه کردن
-              </Button>
-              <Button
-                outline
-                color="secondary"
-                type="reset"
-                onClick={handleReset}
-              >
-                ریست
-              </Button>
-            </div>
-          </CardBody>
-        </Card>
+        {({ values, handleSubmit, handleChange }) => (
+          <Card>
+            <CardHeader>
+              <CardTitle tag="h4">ایجاد دوره جدید </CardTitle>
+            </CardHeader>
+            <CardBody>
+              <form onSubmit={handleSubmit}>
+                <SelectOptions values={values}/>
+              </form>
+
+              <div className="d-flex">
+                <Button className="me-1" color="primary" type="submit">
+                  اضافه کردن
+                </Button>
+                <Button
+                  outline
+                  color="secondary"
+                  type="reset"
+                  onClick={handleReset}
+                >
+                  ریست
+                </Button>
+              </div>
+            </CardBody>
+          </Card>
+        )}
       </Formik>
     );
   }
