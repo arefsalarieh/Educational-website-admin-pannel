e1301475e24e446d5955bba2170028d2ed5cde77
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2023-12-09 21:34:13 +0330

    dsfsf

---
+++ src/pages/GetReserveCourse.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import FormGetCourseReserve from '../@core/components/FormReseveCourse/FormGetCourseReserve'
+
+function GetReserveCourse() {
+  return (
+    <div>
+      <FormGetCourseReserve/>
+    </div>
+  )
+}
+export default GetReserveCourse
